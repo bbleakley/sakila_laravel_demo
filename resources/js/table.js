ab9ae81bcba53f7b0fed841382f436ed610d9f39
Bill <bill@waldensolutions.net>
2024-01-25 01:55:45 +0000

    table sorting

--- resources/js/table.js
+++ resources/js/table.js
@@ -2,6 +2,7 @@ class tableUtil{
 
 	table = document.querySelector("#datatable");
 	tableBody = this.table.querySelector("tbody");
+	tableHead = this.table.querySelector("thead");
 	currentPage = 0;
 	pageSelector;
 	pageLengthSelector;
@@ -19,9 +20,18 @@ class tableUtil{
 		this.table.insertAdjacentHTML("afterend","<div id='pageSelector' class='d-flex flex-row'></div>");
 		this.pageSelector = document.querySelector("#pageSelector");
 		this.pageLengthSelector = document.querySelector("#pageLengthSelect");
+		this.prepTable();
 		// display the default number of rows and trigger listeners
 		this.updateView();
 		this.listen();
+	}
+
+	prepTable(){
+		let i = 0;
+		this.tableHead.querySelectorAll("th").forEach( c => {
+			c.dataset.index = i;
+			i++;
+		});
 	}
 
 	updateView(){
@@ -108,6 +118,8 @@ class tableUtil{
 		this.pageChangeListener();
 		// changes to the number of visible rows per page
 		this.pageLengthChangeListener();
+		// changes to the sort order
+		this.sortListener();
 	}
 
 	pageChangeListener(){
@@ -133,6 +145,49 @@ class tableUtil{
 			this.pageLengthSelector.dataset.value = newLength;
 			this.updateView();
 		});
+	}
+
+	sortListener(){
+		// sort when a column header is clicked
+		this.table.querySelector("thead").addEventListener("click", e =>{
+			let col = e.target.closest("th");
+			let index = +col.dataset.index;
+			let sort = "descTableSort";
+			let img = '<svg class="sortIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M182.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128z"/></svg>';
+			if( col.classList.contains(sort) ){
+				sort = "ascTableSort";
+				img = '<svg class="sortIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M182.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8H288c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z"/></svg>';
+			}
+			// remove existing icon and add new one
+			let existingIcon, descSorted, ascSorted;
+			if( existingIcon = this.tableHead.querySelector(".sortIcon") ){
+				existingIcon.remove();
+			}
+			col.insertAdjacentHTML("beforeend",img);
+			// remove any previously applied sort classes
+			if( ascSorted = this.tableHead.querySelector(".ascTableSort") ){
+				ascSorted.classList.remove("ascTableSort");
+			}
+			if( descSorted = this.tableHead.querySelector(".descTableSort") ){
+				descSorted.classList.remove("descTableSort");
+			}
+			col.classList.add(sort);
+			// sort rows
+			Array.from(this.tableBody.querySelectorAll('tr'))
+			    .sort((a, b) => {
+					let val1 = a.children[index].textContent;
+					let val2 = b.children[index].textContent;
+					let sortModifier = sort == "ascTableSort" ? 1 : -1;
+					// compare as numbers if possible
+					if( ! isNaN(+val1) && ! isNaN(+val2) ){
+						return (+val1 - +val2) * sortModifier;
+					}
+					return val1.toString().localeCompare(val2) * sortModifier;
+				})
+    			.forEach(tr => this.tableBody.appendChild(tr) );
+			this.currentPage = 0;
+			this.updateView();
+		})
 	}
 
 	renderPageDiv(value, extraClasses="", goto=false ){

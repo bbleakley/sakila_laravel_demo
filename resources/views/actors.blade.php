50d3788d5d94a2048d138f83b243e84c969f1a91
Bill <bill@waldensolutions.net>
2024-02-06 20:55:26 +0000

    adding table features to actors page

--- resources/views/actors.blade.php
+++ resources/views/actors.blade.php
@@ -1,6 +1,7 @@
 @extends("layouts.app")
 @section("title","Actors")
 @section("content")
+@vite('resources/js/listView.js')
 <div class="jumbotron">
 	<div class="container">
 		<h1>Actors</h1>
@@ -9,12 +10,16 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xl-12">
-			<table id="datatable" class="table table-striped">
+			<table id="datatable" class="table" data-striped="true">
 				<thead>
 					<tr>
 						<th>Name</th>
 						@foreach( array_keys($actors[0]["details"]) as $h )
-							<th>{{ $h }}</th>
+							@if( $h === "Name" )
+								<th>{{ $h }}</th>
+							@else
+								<th data-sort="desc">{{ $h }}</th>
+							@endif
 						@endforeach
 					</tr>
 				</thead>
